2caffd6be5f6fcf023568ed0ff46c1cf5b86f04e
Victor Shan <victorshanvs@gmail.com>
2020-09-10 15:40:42 -0700

    Presentable navbar (no behavior)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,9 @@
 import NavBar from './NavBar'
-//import styles from '../styles/Layout.module.sass' className={styles.wholePage}
-
 export default function Layout({ children }) {
   return (
-    <div >
+    <>
       <NavBar />
       {children}
-    </div>
+    </>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,8 +2,17 @@ import Navbar from 'react-bootstrap/Navbar'
 
 export default function NavBar() {
   return (
-    <Navbar fixed='top'>
-      <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+    <Navbar fixed='top' bg='light' expand='sm'>
+      <Navbar.Brand href="#home">
+        <img
+          src="/vercel.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />{' '}
+        Navbar with text
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
